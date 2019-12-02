a556cd12f2423532aeb5ea793cbb9727252c075d
bsu-wrudisill <williamrudisill@u.boisestate.edu>
2019-12-01 17:50:50 -0700

    add nldas npy script

---
+++ data_processing/write_hgt_nldas.py
@@ -0,0 +1,59 @@
+from pathlib import Path
+import xarray as xr 
+import numpy as np
+import matplotlib.pyplot as plt 
+import pandas as pd 
+
+#dataDir = Path('/home/wrudisill/Documents/EastRiverPaper/data')
+dataDir = Path('/home/wrudisill/scratch/EastRiverClimatePaper/data')
+# Read geographic data 
+topo = xr.open_dataset(dataDir.joinpath('geog','topography.nc'))
+
+
+# Read precipitation data  
+nldas = xr.open_dataset(dataDir.joinpath('NLDAS','aggregates', '2017NLDAS.nc'))
+
+def applyEastMask(
+	dataset, 
+	var,
+	flattenValues = True,
+	**kwargs):
+	
+	mask = dataDir.joinpath('geog','EastRiverMask.nc')
+	# read in the maskFile 
+	maskFile = xr.open_dataset(mask)
+		
+	# Get a list of vars in the dataset ...    
+	varlist = list(dataset.variables.keys()) 
+
+	# Assign values to the mask array
+	dataset['MASK'] = dataset[var] # create a copy of the var 
+	dataset['MASK'].values = maskFile['T2'].values
+
+	data_mask  = dataset.where(dataset.MASK == 1)
+	data_mask = data_mask[var]
+	# Flatten the values of the array 
+	if flattenValues:
+		flat = data_mask.values.flatten()
+		flat = flat[~np.isnan(flat)]
+		return flat
+	else:
+		twod = data_mask[var]
+		return twod
+
+
+topo = applyEastMask(topo, 'HGT')
+nldas_x = np.empty((365, topo.shape[0]))  # time X points_in_basin
+
+for t, date in enumerate(pd.date_range("2016-10-01", "2017-09-30", freq='1D')):
+	for var in ['TMEAN', 'TMIN', 'TMAX', 'PRCP']:
+		#---- prism ----
+		nldas_t = nldas.sel(time=date)
+		nldas_t = applyEastMask(nldas_t, var)
+		
+		# assign it to the array
+		nldas_x[t, :] = nldas_t
+		
+		# save things ...
+		np.save(dataDir.joinpath('npyfiles','NLDAS_wy2017_daily_east_only_{}'.format(var)), nldas_x)
+	
